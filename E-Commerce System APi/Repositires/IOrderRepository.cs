bb86d39c2b7b6264ebc7c45c2e59ada3ff9cfd21
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-18 11:34:46 +0400

    add OrderRepository

---
+++ E-Commerce System APi/Repositires/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using E_Commerce_System_APi.Models;
+
+namespace E_Commerce_System_APi.Repositires
+{
+    public interface IOrderRepository
+    {
+        void AddOrder(Order order);
+        void AddOrderProduct(OrderProduct orderProduct);
+        Order GetById(int id);
+        List<Order> GetByUserId(int userId);
+    }
+}
---
+++ E-Commerce System APi/Repositires/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using E_Commerce_System_APi.Models;
+using System;
+
+namespace E_Commerce_System_APi.Repositires
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Order GetById(int id)
+        {
+            return _context.Orders.SingleOrDefault(o => o.OID == id);
+        }
+
+        public List<Order> GetByUserId(int userId)
+        {
+            return _context.Orders.Where(o => o.UserId == userId).ToList();
+        }
+
+        public void AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+
+        public void AddOrderProduct(OrderProduct orderProduct)
+        {
+            _context.OrderProducts.Add(orderProduct);
+            _context.SaveChanges();
+        }
+    }
+}
+
+
