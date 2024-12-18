87c2b21e48adc1b40bb4c08e3ea1f51be24576f0
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-18 12:36:16 +0400

    add ReviewService

---
+++ E-Commerce System APi/Services/IReviewService.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce_System_APi.DTO;
+using E_Commerce_System_APi.Models;
+
+namespace E_Commerce_System_APi.Services
+{
+    public interface IReviewService
+    {
+        Review AddReview(ReviewDto model, int userId);
+        Review UpdateReview(int reviewId, ReviewDto model, int userId);
+    }
+}
