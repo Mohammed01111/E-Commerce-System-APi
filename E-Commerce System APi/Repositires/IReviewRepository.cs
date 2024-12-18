700aea9048b836714aa07ecc3bfd121da06bb9e5
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-18 12:31:33 +0400

    add ReviewRepository

---
+++ E-Commerce System APi/Repositires/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce_System_APi.Models;
+
+namespace E_Commerce_System_APi.Repositires
+{
+    public interface IReviewRepository
+    {
+        void AddReview(Review review);
+        Review GetById(int reviewId);
+        Review GetReviewByUserAndProduct(int userId, int productId);
+        IEnumerable<Review> GetReviewsByProductId(int productId);
+        void UpdateReview(Review review);
+    }
+}
---
+++ E-Commerce System APi/Repositires/ReviewRepository.cs
@@ -0,0 +1,44 @@
+﻿using E_Commerce_System_APi.Models;
+using System;
+
+namespace E_Commerce_System_APi.Repositires
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ReviewRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddReview(Review review)
+        {
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+        }
+
+        public Review GetById(int reviewId)
+        {
+            return _context.Reviews.FirstOrDefault(r => r.RID == reviewId);
+        }
+
+        public Review GetReviewByUserAndProduct(int userId, int productId)
+        {
+            return _context.Reviews.SingleOrDefault(r => r.UserId == userId && r.ProductId == productId);
+        }
+
+        public IEnumerable<Review> GetReviewsByProductId(int productId)
+        {
+            return _context.Reviews
+                .Where(r => r.ProductId == productId)
+                .ToList();
+        }
+
+        // Update an existing review
+        public void UpdateReview(Review review)
+        {
+            _context.Reviews.Update(review);
+            _context.SaveChanges();
+        }
+    }
+}
